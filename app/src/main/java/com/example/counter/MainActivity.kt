9b578b0e605d3bcbb8bc3c18142f1834c5f0c79e
ianYang <s1081694@gm.pu.edu.tw>
2021-03-19 15:51:34 +0800

    button的OnClick事件

---
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    var counter:Int=0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv.text = counter.toString()
+    }
+    fun Add(v:View){
+        if(v.id == R.id.btn2){
+            counter++
+        }
+        else{
+            counter*=2
+        }
+        txv.text = counter.toString()
+    }
+}
