dbbab929d82aa0da18bf3fad3279d596404be9a0
ianyang <s1081694@gm.pu.edu.tw>
2021-03-26 15:23:18 +0800

    樂透

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -7,20 +7,39 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+
     var counter:Int=0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txv.text = counter.toString()
+        txv.setOnClickListener(this)
+        btn2.setOnClickListener(this)
+        btn3.setOnClickListener(this)
+        btn4.setOnClickListener(this)
+
+        btnHappy.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+             counter = (1..100).random()
+                txv.text = counter.toString()
+            }
+        })
+
     }
-    fun Add(v:View){
-        //if(v.id == R.id.btn2){
-        if(v == btn2){
+
+
+    override fun onClick(v: View?) {
+        if(v==btn2){
             counter++
+        }else if(v==btn3){
+            counter +=2
         }
         else{
-            counter*=2
+            counter = 0
         }
         txv.text = counter.toString()
     }
